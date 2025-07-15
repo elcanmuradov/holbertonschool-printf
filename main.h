325deb7e3dca14310b165f21aac2183a9ce20a02
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-15 16:03:21 +0000

    4

--- main.h
+++ main.h
@@ -1,15 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...)
 {
+int printed = 0;
 int size = 0;
 int i = 0;
-int j = 0;
 va_list args;
 va_start(args, format);
 
@@ -22,21 +21,25 @@ if (format[size] == 'c')
 {
 char c = (char)va_arg(args, int);
 write(1, &c, 1);
+printed++;
 }
 else if (format[size] == 'd' || format[size] == 'i')
 {
 int num = va_arg(args, int);
 char buffer[12];
 i = 0;
+
 if (num == 0)
+{
 buffer[i++] = '0';
+}
 else
 {
 int neg = 0;
 if (num < 0)
 {
 neg = 1;
-num *= -1;
+num = -num; 
 }
 
 while (num > 0)
@@ -47,7 +50,9 @@ num /= 10;
 if (neg)
 buffer[i++] = '-';
 }
-while ( j < i / 2)
+
+int j = 0;  
+while (j < i / 2)
 {
 char tmp = buffer[j];
 buffer[j] = buffer[i - j - 1];
@@ -56,11 +61,13 @@ j++;
 }
 
 write(1, buffer, i);
+printed += i;
 i = 0;
 }
 else if (format[size] == '%')
 {
 write(1, "%", 1);
+printed++;
 }
 else
 {
@@ -69,12 +76,12 @@ else
 else
 {
 write(1, &format[size], 1);
+printed++;
 }
 size++;
 }
 
 va_end(args);
-return size;
+return printed;
 }
-
 #endif /* MAIN_H */

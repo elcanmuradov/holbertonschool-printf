76d3c3f34a48049c41975193ba1d48fe04af30cc
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-15 15:37:15 +0000

    4

--- main.h
+++ main.h
@@ -1,82 +1,79 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
+
 #include <stdarg.h>
+#include <unistd.h>
 #include <stdlib.h>
+
 int _printf(const char *format, ...)
 {
-int size = 0;
-int num,i;
-char *qaliq,*arrnum;
-int *intpr;
-char *chrpr;
 va_list args;
-va_start(args,format);
-while(format[size] != '\0')
+va_start(args, format);
+
+int size = 0;
+
+while (format && format[size])
 {
-if (format[size] == '%'){
-if (format[size + 1] == 'd' || format[size + 1] == 'i'){
-intpr = malloc(sizeof(va_arg(args,int)));
-write(1,intpr,sizeof(intpr));
-size++;
+if (format[size] == '%')
+{
+size++; // '%' geç
+if (format[size] == 'c')
+{
+char c = (char)va_arg(args, int);
+write(1, &c, 1);
 }
-else if (format[size + 1] == 'c'){
-chrpr = malloc(sizeof(va_arg(args,int)));
-write(1,chrpr,1);
-size++;
+else if (format[size] == 'd' || format[size] == 'i')
+{
+int num = va_arg(args, int);
+char buffer[12];
+int i = 0;
+
+if (num == 0)
+buffer[i++] = '0';
+else
+{
+int neg = 0;
+if (num < 0)
+{
+neg = 1;
+num *= -1;
 }
-else if (format[size + 1] == 'p'){
-chrpr = malloc(sizeof(va_arg(args,int)));
-write(1,&chrpr,sizeof(chrpr));
-size++;
-}
-else if (format[size + 1] == 'X' || format[size + 1] == 'x'){
-intpr = malloc(sizeof(va_arg(args,int)));
-num = va_arg(args,int);
-i = 0;
-*qaliq = "0123456789abcdef";
+
 while (num > 0)
 {
-arrnum[i] = num%16;
-num = num/16;
-i++;
+buffer[i++] = (num % 10) + '0';
+num /= 10;
 }
-num = atoi(arrnum);
-write(1,num,sizeof(num));
-size++;
+
+if (neg)
+buffer[i++] = '-';
 }
-else if (format[size + 1] == 'O' || format[size + 1] == 'o'){
-intpr = malloc(sizeof(va_arg(args,int)));
-num = va_arg(args,int);
-*arrnum;
-i = 0;
- *qaliq = "01234567";
-while (num > 0)
+for (int j = 0; j < i / 2; j++)
 {
-arrnum[i] = num%8;
-num = num/8;
-i++;
+char tmp = buffer[j];
+buffer[j] = buffer[i - j - 1];
+buffer[i - j - 1] = tmp;
 }
-size++;
+
+write(1, buffer, i);
 }
-else if (format[size + 1] == 'u'){
-intpr = malloc(sizeof(va_arg(args,int)));
-write(1,va_arg(args,unsigned int),sizeof(unsigned int));
-size++;
+else if (format[size] == '%')
+{
+write(1, "%", 1);
 }
-else if (format[size + 1] == '%'){
-writde(1,'%',1);
-size++;
-}else{
-write(1,format[size],1);
+else
+{
 }
 }
-else{
-write(1,format[size],1);
+else
+{
+write(1, &format[size], 1);
 }
 size++;
 }
 
+va_end(args);
+return size;
 }
 
 #endif /* MAIN_H */
